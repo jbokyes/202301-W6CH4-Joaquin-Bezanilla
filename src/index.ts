b638614c9c384ef722eb9e8275e87cb228d95264
Joaquin Bezanilla <joaquinbezanilla@gmail.com>
2023-02-23 20:04:23 +0100

    Add server and calculator

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+/* eslint-disable @typescript-eslint/comma-dangle */
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
+
+/* eslint-disable @typescript-eslint/naming-convention */
+/* eslint-disable @typescript-eslint/object-curly-spacing */
+import http from 'http';
+import url from 'url';
+
+const PORT = process.env.PORT ?? '4300';
+
+const server = http.createServer((req, resp) => {
+	if (req.method === 'GET') {
+		if (!req.url) {
+			server.emit('error', new Error('Error 404 😜'));
+			return;
+		}
+
+		const { pathname } = url.parse(req.url);
+		if (pathname !== '/calculator') {
+			server.emit('error', new Error('bruh'));
+			return;
+		}
+
+		if (pathname === '/calculator') {
+			const URLParams = new URL(
+				req.url,
+				`http://${req.headers.hostname}/calculator`
+			);
+			const query = URLParams.searchParams;
+			const n1 = Number(query.get('a'));
+			const n2 = Number(query.get('b'));
+			const sum = n1 + n2;
+			const rest = n1 - n2;
+			const multiply = n1 * n2;
+			const divide = n1 / n2;
+			resp.writeHead(300, { 'Content-Type': 'text/html' });
+			resp.write(`
+          <html>
+            <head>
+              <title>Calculator</title>
+            </head>
+            <body>
+              <h1>Calculator Result</h1>
+              <p>${n1} + ${n2} = ${sum}</p>
+              <p>${n1} - ${n2} = ${rest}</p>
+              <p>${n1} * ${n2} = ${multiply}</p>
+              <p>${n1} / ${n2} = ${divide}</p>
+            </body>
+          </html>`);
+		}
+	}
+});
+
+server.listen(PORT);
